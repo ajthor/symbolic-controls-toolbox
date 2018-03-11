e40e0936538c12401cc2ef738045c84ea35fcef1
adam <adam.thorpe.g@gmail.com>
2018-03-10 21:41:01 -0600

    Working commit.

--- ctrlmodels/private/getSymbolicDerivatives.m
+++ ctrlmodels/private/getSymbolicDerivatives.m
@@ -2,26 +2,25 @@ function D = getSymbolicDerivatives(expr)
 %GETSYMBOLICDERIVATIVES Finds all symbolic derivatives in expression.
 %   Detailed explanation goes here
 
-syms t
-vars = symvar(expr);
-vars = vars(vars ~= t);
+% syms t
+% vars = symvar(expr);
+% vars = vars(vars ~= t);
 
 ch = expr;
 
-safety = 0;
-
 % Isolate derivatives by removing all terms that do not contain
 % derivatives.
-while nnz(has(ch, vars)) ~= 0
-    ch = children(ch);
-    ch = cell2sym(ch);
-    
-    ch = ch(has(ch, {'diff', 'int'}));
-    
-    safety = safety + 1;
-    if safety == 10
-        break
+while true
+    nch = children(ch);
+    if iscell(nch)
+        nch = cell2sym(nch);
     end
+    
+    if ~any(has(nch, {'diff', 'int'}))
+        break;
+    end
+    
+    ch = nch(has(nch, {'diff', 'int'}));
 end
 
 D = ch;
