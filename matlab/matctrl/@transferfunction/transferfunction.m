11b49a9f6c305438312d1f783bd944386dac5ae3
adam <adam.thorpe.g@gmail.com>
2018-11-09 12:15:38 -0700

    Add overloaded operators for transfer functions.

--- matlab/matctrl/@transferfunction/transferfunction.m
+++ matlab/matctrl/@transferfunction/transferfunction.m
@@ -134,4 +134,123 @@ classdef (SupportExtensionMethods = true, InferiorClasses = {?sym}) transferfunc
         end
     end
 
+    % Overloaded operators.
+    methods
+        function C = plus(A, B)
+            %PLUS Overloaded to allow for transfer functions to be added
+            %without pole-zero cancellations.
+            C = transferfunction();
+            if ~isa(A, 'transferfunction')
+                G = B;
+                H = A;
+            else
+                G = A;
+                H = B;
+            end
+
+            C.var = G.var;
+
+            if ~isa(H, 'transferfunction')
+                if ~isa(H, 'sym')
+                    H = sym(H);
+                end
+
+                [N, D] = numden(H);
+                C.num = G.num*D + N*G.den;
+                C.den = G.den*D;
+            else
+                if G.var ~= H.var
+                    error('Transfer functions must have the same variable.');
+                end
+                C.num = G.num*H.den + H.num*G.den;
+                C.den = G.den*H.den;
+            end
+        end
+
+        function C = mtimes(A, B)
+            %MTIMES Overloaded to allow for transfer functions to be
+            %multiplied without pole-zero cancellations.
+            C = transferfunction();
+            if ~isa(A, 'transferfunction')
+                G = B;
+                H = A;
+            else
+                G = A;
+                H = B;
+            end
+
+            C.var = G.var;
+
+            if ~isa(H, 'transferfunction')
+                if ~isa(H, 'sym')
+                    H = sym(H);
+                end
+
+                [N, D] = numden(H);
+                C.num = G.num*N;
+                C.den = G.den*D;
+            else
+                if G.var ~= H.var
+                    error('Transfer functions must have the same variable.');
+                end
+                C.num = G.num*H.num;
+                C.den = G.den*H.den;
+            end
+        end
+
+        function C = mrdivide(A, B)
+            %MRDIVIDE Overloaded to allow for transfer functions to be
+            %divided without pole-zero cancellations.
+            C = transferfunction();
+            if ~isa(A, 'transferfunction')
+                G = B;
+                G.num = B.den;
+                G.den = B.num;
+                H = A;
+            else
+                G = A;
+                H = B;
+            end
+
+            C.var = G.var;
+
+            if ~isa(H, 'transferfunction')
+                C.num = G.num;
+                C.den = G.den*H;
+            else
+                if G.var ~= H.var
+                    error('Transfer functions must have the same variable.');
+                end
+                C.num = G.num*H.den;
+                C.den = G.den*H.num;
+            end
+        end
+
+        function C = mpower(A, B)
+            %MPOWER Overloaded to allow for transfer functions to use the
+            %matrix power operator without pole-zero cancellations.
+            C = transferfunction();
+            if ~isa(A, 'transferfunction')
+                G = B;
+                H = A;
+            else
+                G = A;
+                H = B;
+            end
+
+            C.var = G.var;
+
+            if ~isa(H, 'transferfunction')
+                C.num = G.num^H;
+                C.den = G.den^H;
+            else
+                if G.var ~= H.var
+                    error('Transfer functions must have the same variable.');
+                end
+
+                C.num = G.num^H;
+                C.den = G.den^H;
+            end
+        end
+    end
 end
--- matlab/test/test_transfer_function.m
+++ matlab/test/test_transfer_function.m
@@ -78,5 +78,76 @@ classdef test_transfer_function < matlab.unittest.TestCase
             testCase.verifyEqual(sys.den(1), [s + 1]);
             testCase.verifyEqual(sys.den(2), [s - 1]);
         end
+
+        function testTransferFunctionAdd(testCase)
+            syms s
+
+            sys1 = transferfunction();
+            sys1.var = s;
+            sys2 = transferfunction();
+            sys2.var = s;
+
+            sys1.num = s;
+            sys1.den = 1;
+
+            sys2.num = s;
+            sys2.den = 1;
+
+            sys1 = sys1 + sys2;
+            testCase.verifyEqual(sys1.num, 2*s);
+            testCase.verifyEqual(sys1.den, 1);
+        end
+
+        function testTransferFunctionMul(testCase)
+            syms s
+
+            sys1 = transferfunction();
+            sys1.var = s;
+            sys2 = transferfunction();
+            sys2.var = s;
+
+            sys1.num = s;
+            sys1.den = 1;
+
+            sys2.num = s;
+            sys2.den = 1;
+
+            sys1 = sys1 * sys2;
+            testCase.verifyEqual(sys1.num, s^2);
+            testCase.verifyEqual(sys1.den, 1);
+        end
+
+        function testTransferFunctionDiv(testCase)
+            syms s
+
+            sys1 = transferfunction();
+            sys1.var = s;
+            sys2 = transferfunction();
+            sys2.var = s;
+
+            sys1.num = s;
+            sys1.den = 1;
+
+            sys2.num = s^2;
+            sys2.den = 1;
+
+            sys1 = sys1/sys2;
+            testCase.verifyEqual(sys1.num, s);
+            testCase.verifyEqual(sys1.den, s^2);
+        end
+
+        function testTransferFunctionPow(testCase)
+            syms s
+
+            sys = transferfunction();
+            sys.var = s;
+
+            sys.num = s;
+            sys.den = 1;
+
+            sys = sys^2;
+            testCase.verifyEqual(sys.num, s^2);
+            testCase.verifyEqual(sys.den, 1);
+        end
     end
 end
