78cba6000e5d6ddff16e45610b64195cf8abf4b9
adam <adam.thorpe.g@gmail.com>
2018-12-30 15:40:24 -0700

    Modify constant matrix template.

--- math/matrix/constant/constant.hpp
+++ math/matrix/constant/constant.hpp
@@ -11,14 +11,12 @@ namespace Math {
 // ----------------------------------------------------------------------
 // ConstantMatrix
 //
-template<typename T, size_t N, size_t M>
-class ConstantMatrix : public Matrix<ConstantMatrix<T, N, M>> {
+template<typename T, size_t N, size_t M, T v_>
+class ConstantMatrix : public Matrix<ConstantMatrix<T, N, M, v_>> {
 private:
-  const T v_;
 
 public:
-  ConstantMatrix(const T v);
-  // ConstantMatrix(const std::vector<T> v);
+  ConstantMatrix();
 
   inline size_t size() const;
   inline size_t capacity() const;
@@ -38,74 +36,68 @@ public:
   inline const T operator()(const size_t row, const size_t col) const;
 };
 
-template<typename T, size_t N, size_t M>
-ConstantMatrix<T, N, M>::ConstantMatrix(const T v) : v_(v) {
+template<typename T, size_t N, size_t M, T v_>
+ConstantMatrix<T, N, M, v_>::ConstantMatrix() {
   //
-  // v_ = std::vector<T>(N*M, v);
 }
 
-// template<typename T, size_t N, size_t M>
-// ConstantMatrix<T, N, M>::ConstantMatrix(const std::vector<T> v) : v_(v) {
-//   //
-// }
-
-template<typename T, size_t N, size_t M>
-inline size_t ConstantMatrix<T, N, M>::size() const {
+template<typename T, size_t N, size_t M, T v_>
+inline size_t ConstantMatrix<T, N, M, v_>::size() const {
   return N*M;
 }
 
-template<typename T, size_t N, size_t M>
-inline size_t ConstantMatrix<T, N, M>::capacity() const {
+template<typename T, size_t N, size_t M, T v_>
+inline size_t ConstantMatrix<T, N, M, v_>::capacity() const {
   return 0;
 }
 
-template<typename T, size_t N, size_t M>
-inline bool ConstantMatrix<T, N, M>::empty() const {
+template<typename T, size_t N, size_t M, T v_>
+inline bool ConstantMatrix<T, N, M, v_>::empty() const {
   return false;
 }
 
-template<typename T, size_t N, size_t M>
-inline size_t ConstantMatrix<T, N, M>::nrows() const {
+template<typename T, size_t N, size_t M, T v_>
+inline size_t ConstantMatrix<T, N, M, v_>::nrows() const {
   return N;
 }
 
-template<typename T, size_t N, size_t M>
-inline size_t ConstantMatrix<T, N, M>::ncols() const {
+template<typename T, size_t N, size_t M, T v_>
+inline size_t ConstantMatrix<T, N, M, v_>::ncols() const {
   return M;
 }
 
-template<typename T, size_t N, size_t M>
-inline std::vector<T> ConstantMatrix<T, N, M>::as_vec() const {
+template<typename T, size_t N, size_t M, T v_>
+inline std::vector<T> ConstantMatrix<T, N, M, v_>::as_vec() const {
   std::vector<T> v(N*M, v_);
 
   return v;
 }
 
-template<typename T, size_t N, size_t M>
-inline DenseMatrix<T> ConstantMatrix<T, N, M>::as_dense() const {
+template<typename T, size_t N, size_t M, T v_>
+inline DenseMatrix<T> ConstantMatrix<T, N, M, v_>::as_dense() const {
   std::vector<T> v = (*this).as_vec();
 
   return DenseMatrix<T>(N, M, v);
 }
 
-template<typename T, size_t N, size_t M>
-inline T ConstantMatrix<T, N, M>::operator[](const size_t pos) {
+template<typename T, size_t N, size_t M, T v_>
+inline T ConstantMatrix<T, N, M, v_>::operator[](const size_t pos) {
   return v_;
 }
 
-template<typename T, size_t N, size_t M>
-inline const T ConstantMatrix<T, N, M>::operator[](const size_t pos) const {
+template<typename T, size_t N, size_t M, T v_>
+inline const T ConstantMatrix<T, N, M, v_>::operator[](const size_t pos) const {
   return v_;
 }
 
-template<typename T, size_t N, size_t M>
-inline T ConstantMatrix<T, N, M>::operator()(const size_t row,
+template<typename T, size_t N, size_t M, T v_>
+inline T ConstantMatrix<T, N, M, v_>::operator()(const size_t row,
                                              const size_t col) {
   return v_;
 }
 
-template<typename T, size_t N, size_t M>
-inline const T ConstantMatrix<T, N, M>::operator()(const size_t row,
+template<typename T, size_t N, size_t M, T v_>
+inline const T ConstantMatrix<T, N, M, v_>::operator()(const size_t row,
                                                    const size_t col) const {
   return v_;
 }
